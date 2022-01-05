64ec6f50b33b4552db377ec7c88084515861341f
ikkyu <389498623@qq.com>
2022-01-06 04:44:22 +0800

    chore: upoad changes

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package openrpc
 
-import "context"
+import (
+	"context"
+	"reflect"
+)
 
 type MethodDesc struct {
 	Name        string
@@ -20,4 +23,25 @@ type ServiceDesc struct {
 type ParameterDesc struct {
 	Name        string
 	Description string
+}
+
+type Server struct {
+	// The service descriptor.
+	ServiceDesc ServiceDesc
+	// The method descriptors.
+	MethodDescs []MethodDesc
+}
+
+func (s *Server) Serve(ctx context.Context, server interface{}) error {
+	return nil
+}
+
+func (s *Server) RegisterService(sd *ServiceDesc, impl interface{}) error {
+	if impl != nil {
+		s.ServiceDesc = *sd
+	}
+	ht := reflect.TypeOf(sd.HandlerType).Elem()
+	st := reflect.TypeOf(impl)
+    if !st.Implements(ht) {
+	return nil
 }
